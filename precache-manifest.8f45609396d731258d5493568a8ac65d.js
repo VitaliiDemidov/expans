366c3cffd27f09d70d875f074fc90ef1cce52d72
vitaliidemidov <demidov1@i.ua>
2020-07-07 17:52:24 +0300

    Updates

---
+++ precache-manifest.8f45609396d731258d5493568a8ac65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb13d56f7f80a224961d0c3f789dda14",
+    "url": "/expans/index.html"
+  },
+  {
+    "revision": "4b9d8107b7c8be5a1dc2",
+    "url": "/expans/static/css/main.94ddb379.chunk.css"
+  },
+  {
+    "revision": "f0476b2aca3213d1c53e",
+    "url": "/expans/static/js/2.e31ce1b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expans/static/js/2.e31ce1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9d8107b7c8be5a1dc2",
+    "url": "/expans/static/js/main.a88df2ab.chunk.js"
+  },
+  {
+    "revision": "fccef7e14e14a14586e3",
+    "url": "/expans/static/js/runtime-main.e73e988e.js"
+  }
+]);
