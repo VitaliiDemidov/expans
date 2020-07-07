585be1f8b8da54dec6cd6b6f530ae6bf9d05ebaa
vitaliidemidov <demidov1@i.ua>
2020-07-07 17:15:16 +0300

    deploy

---
+++ build/precache-manifest.151069357a0e0f680331b99508078ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d260208274a926381214bb28da7a100",
+    "url": "/expanses-testWork/index.html"
+  },
+  {
+    "revision": "4b9d8107b7c8be5a1dc2",
+    "url": "/expanses-testWork/static/css/main.94ddb379.chunk.css"
+  },
+  {
+    "revision": "f0476b2aca3213d1c53e",
+    "url": "/expanses-testWork/static/js/2.e31ce1b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expanses-testWork/static/js/2.e31ce1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9d8107b7c8be5a1dc2",
+    "url": "/expanses-testWork/static/js/main.a88df2ab.chunk.js"
+  },
+  {
+    "revision": "abd171b69ed1280efd43",
+    "url": "/expanses-testWork/static/js/runtime-main.9632febe.js"
+  }
+]);
